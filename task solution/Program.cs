b2d0cb12283755fa56819c8210cceca2f4eec596
Maksim Demakin <maxxdemakin@gmail.com>
2022-12-25 14:33:51 +0700

    Добавил решение задачи на C#

---
+++ task solution/Program.cs
@@ -0,0 +1,39 @@
+﻿int NumberRequiredElements (string [] array)
+{
+    int size = 0; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        size++;
+    }
+    return size;
+}
+
+string [] СreatingNewArray (string [] array, int size)
+{
+    string [] newArray = new string [size];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray (string [] newArray)
+{
+    for (int j = 0; j < newArray.Length; j++)
+    {
+        Console.Write($" {newArray[j]}");
+    }
+    Console.WriteLine();
+}
+
+string [] array = new string [] {"Tom","Sam","Bob","Maksim","Jonny","Max"};
+PrintArray(array);
+PrintArray(СreatingNewArray(array, NumberRequiredElements(array)));
